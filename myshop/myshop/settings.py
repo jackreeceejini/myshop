26f19b48728d3d757b995b60db0feec5c2db3498
jackreece Ejini <jackreeceejini@gmail.com>
2022-04-12 16:43:42 -0400

    added recommendation system

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -181,3 +181,8 @@ PARLER_LANGUAGES = {
         'hide_untranslated': False, 
     }
 }
+
+# Redis
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379 
+REDIS_DB = 1
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,8 @@
+from math import prod
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product 
 from cart.forms import CartAddProductForm 
+from .recommender import Recommender 
 
 def product_list(request, category_slug=None):
     category = None 
@@ -22,7 +24,11 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, 
     translations__language_code=language, translations__slug=slug,available=True)
     cart_product_form = CartAddProductForm()
+
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
     return render(request, 'shop/product/detail.html', 
     {'product': product,
-    'cart_product_form': cart_product_form
+    'cart_product_form': cart_product_form,
+    'recommended_products': recommended_products
     })
